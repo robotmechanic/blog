f46b2c056c29b72e7d59a2fa27a90e0c0bcaff63
David Hovanky <d.hovanky@axelmark.co.jp>
2014-10-17 16:04:38 +0900

    nokogiri wiki test

--- scripts/nokogiri.rb
+++ scripts/nokogiri.rb
@@ -1,3 +1,5 @@
+#http://ruby.bastardsbook.com/chapters/web-crawling/
+
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
@@ -29,3 +31,24 @@ links = page.css("a")
 puts links.length.to_s + " links found."  # => 6
 puts links[0].text   # => Click here
 puts links[0]["href"] # => http://www.google.com
+
+BASE_WIKIPEDIA_URL = "http://en.wikipedia.org"
+LIST_URL = "#{BASE_WIKIPEDIA_URL}/wiki/List_of_Nobel_laureates"
+page = Nokogiri::HTML(open(LIST_URL))
+rows = page.css('div.mw-content-ltr table.wikitable tr')         
+
+rows[1..-2].each do |row|
+  hrefs = row.css("td a").map{ |a| 
+    a['href'] if a['href'].match("/wiki/")
+  }.compact.uniq
+
+  hrefs.each do |href|
+   puts href
+  end
+ 
+ hrefs.each do |href|
+    remote_url = BASE_WIKIPEDIA_URL + href
+    puts remote_url    
+  end # done: hrefs.each
+
+end # done: rows.each
